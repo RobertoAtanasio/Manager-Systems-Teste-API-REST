d347ea41b4a78892d723a14359e7ed330db02c57
Roberto Atanásio <roberto.atanasio.pl@gmail.com>
2019-06-19 14:31:54 -0300

    incluir classe @ExceptionHandler nas API

--- src/main/java/com/managersystem/sisclinica/api/resource/PaisResource.java
+++ src/main/java/com/managersystem/sisclinica/api/resource/PaisResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.managersystem.sisclinica.api.exception.PaisInexistenteException;
+import com.managersystem.sisclinica.api.exception.PaisJaExistenteException;
 import com.managersystem.sisclinica.api.exception.TokenExpiradoException;
 import com.managersystem.sisclinica.api.exception.TokenInexistenteException;
 import com.managersystem.sisclinica.api.exception.TokenNaoAdministradorException;
@@ -34,24 +36,14 @@ public class PaisResource {
 	@GetMapping("/pesquisar")
 	public ResponseEntity<List<Pais>> pesquisar(PaisFiltro paisFiltro) {
 		List<Pais> lista = null;
-		try {
-			lista = paisService.pesquisar(paisFiltro.getToken(), paisFiltro);		
-		} catch (TokenInexistenteException e) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-		}
+		lista = paisService.pesquisar(paisFiltro.getToken(), paisFiltro);		
 		return ResponseEntity.status(HttpStatus.OK).body(lista);
 	}
 	
 	@GetMapping("/listar")
 	public ResponseEntity<List<Pais>> listar(TokenFiltro tokenFiltro) { 
 		List<Pais> lista = null;
-		try {
-			lista = paisService.listar(tokenFiltro.getToken());
-		} catch (TokenInexistenteException e) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-		} catch (TokenExpiradoException e) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-		}
+		lista = paisService.listar(tokenFiltro.getToken());
 		return ResponseEntity.status(HttpStatus.OK).body(lista);
 	}
 	
@@ -59,46 +51,43 @@ public class PaisResource {
 	public ResponseEntity<Pais> salvar(TokenFiltro tokenFiltro, @Valid @RequestBody Pais pais, HttpServletResponse response) {
 		Pais paisSalvo = null;
 		if (pais.getId() > 0) {
-			try {
-				paisSalvo = paisService.atualizar(tokenFiltro.getToken(), pais);
-			} catch (TokenInexistenteException e) {
-				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-			} catch (TokenExpiradoException e) {
-				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-			} catch (TokenNaoAdministradorException e) {
-				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-			} catch (PaisInexistenteException e) {
-				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-			}
+			paisSalvo = paisService.atualizar(tokenFiltro.getToken(), pais);
 			return ResponseEntity.status(HttpStatus.OK).body(paisSalvo);
-		} else {
-			try {				
-				paisSalvo = paisService.salvar(tokenFiltro.getToken(), pais);
-			} catch (TokenInexistenteException e) {
-				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-			} catch (TokenExpiradoException e) {
-				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-			} catch (TokenNaoAdministradorException e) {
-				return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-			}
+		} else {				
+			paisSalvo = paisService.salvar(tokenFiltro.getToken(), pais);
 			return ResponseEntity.status(HttpStatus.CREATED).body(paisSalvo);
 		}		
 	}
-		
+	
 	@GetMapping("/excluir")
 	public ResponseEntity<Object> remover(PaisFiltroExcluir filtroExcluir, HttpServletResponse response) {
-		try {
-			paisService.excluir(filtroExcluir);
-		} catch (PaisInexistenteException e) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-		} catch (TokenNaoAdministradorException e) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-		} catch (TokenInexistenteException e) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-		} catch (TokenExpiradoException e) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
-		}
+		paisService.excluir(filtroExcluir);
 		return ResponseEntity.status(HttpStatus.OK).body(true);
+	}
+	
+	@ExceptionHandler({ TokenNaoAdministradorException.class })
+	public ResponseEntity<Object> handleTokenNaoAdministradorException(TokenNaoAdministradorException ex) {
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+	}
+	
+	@ExceptionHandler({ PaisInexistenteException.class })
+	public ResponseEntity<Object> handlePaisInexistenteException(PaisInexistenteException ex) {
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+	}
+	
+	@ExceptionHandler({ TokenInexistenteException.class })
+	public ResponseEntity<Object> handleTokenInexistenteException(TokenInexistenteException ex) {
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+	}
+	
+	@ExceptionHandler({ TokenExpiradoException.class })
+	public ResponseEntity<Object> handleTokenExpiradoException(TokenExpiradoException ex) {
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+	}
+	
+	@ExceptionHandler({ PaisJaExistenteException.class })
+	public ResponseEntity<Object> handlePaisJaExistenteException(PaisJaExistenteException ex) {
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
 	}
 	
 }
